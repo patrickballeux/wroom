8bf004d1640415d242608c9b607b741f55e0218b
patri <patri@192.168.0.108>
2017-09-30 08:45:59 -0400

    Added relative paths for ceiling and floor

--- src/webroom/WebRoom.java
+++ src/webroom/WebRoom.java
@@ -11,7 +11,7 @@ package webroom;
  */
 public class WebRoom {
 
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2";
     public static boolean NOCHAT = false;
 
 }
